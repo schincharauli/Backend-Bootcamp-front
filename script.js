c27565f7b1d45173619d90a2024d0ec755c68a7a
schincharauli <s.tchintcharauli@gmail.com>
2025-10-02 21:42:26 +0400

    feat: added loginv request and token

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ const addUser = async (event, form) => {
     const response = await instance.post("/users", {
       name: form.name.value,
       email: form.email.value,
+      password: form.password.value,
     });
     console.log(response);
   } catch (error) {}
@@ -59,10 +60,29 @@ const updateUser = async (event, form) => {
 
 const deleteUser = async (id) => {
   try {
-    const id = "21fe6ad8-0a3a-4f2a-93c0-72bb7ee89aa7";
-    const response = await instance.delete(`/users/${id}`);
+    const id = "84a17d45-d185-4f30-886a-5d801b38fc92";
+    const response = await instance.delete(`/users/${id}`, {
+      headers: {
+        authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4ZGVhOTIyNjU5YmI5YWYwNzYzYzhhMiIsImVtYWlsIjoiY2hpbmNoYXJhdWxpNDFAZ21haWwuY29tIiwiaWF0IjoxNzU5NDI1ODgzLCJleHAiOjE3NTk0Mjk0ODN9.yt0JDdm_GX09jlLfe4bdxIsatfhd786VEYk7-273wGw",
+      },
+    });
+
     console.log("user Deleted Succesfully", response.data);
   } catch (error) {
     console.error("Error Deleting user:", error.message);
+  }
+};
+
+const login = async (event, form) => {
+  event.preventDefault();
+  try {
+    const response = await instance.post("login", {
+      email: form.email.value,
+      password: form.password.value,
+    });
+    console.log("Login Success", response.data);
+  } catch (error) {
+    console.error("Error Login user:", error.message);
   }
 };
